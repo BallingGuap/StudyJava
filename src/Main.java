40064630fa074111f6f43c958ca97e67dcb8f83d
sidiropulometin@gmail.com <sidiropulometin@gmail.com>
2023-03-31 19:36:32 +0600

    Homework №2

--- src/Main.java
+++ src/Main.java
@@ -3,77 +3,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Task1();
-        Task2();
     }
 
+   public static void Task1() {
+       for(;;) {
+           Scanner scanner = new Scanner(System.in);
+           System.out.println("Type password:   ");
+           String pass = scanner.nextLine();
 
+           if(pass.length()  < 10){
+               System.out.println("Uncorrected password: " + pass);
+               continue;
+           }
+           if(hasSpecialSymbols(pass)){
+               System.out.println("Uncorrected password: " + pass);
+               continue;
+           }
+           if(countNumbers(pass) < 2){
+               System.out.println("Uncorrected password: " + pass);
+               continue;
+           }
+           System.out.println("Correct password: " + pass);
+       }
+   }
 
-    public  static void Task2()
-    {
-        int bulbs = 0;
-        Scanner scanner = new Scanner(System.in);
-        for(;;) {
-        var choose = scanner.nextInt();
-            switch (choose) {
-                case 1:
-                    for (var i = 0; i < 10; ++i) {
-                        bulbs = blink(bulbs);
-                        showBulbs(bulbs);
-                    }
-                    break;
-                case 2:
-                    System.out.println(isFirstLampOn(bulbs));
-                    break;
-                case 3:
-                    for (var i = 0; i < 10; ++i) {
-                        bulbs = run(bulbs);
-                        showBulbs(bulbs);
-                    }
-                    break;
-                case 4:
-                    showBulbs(bulbs);
-                    break;
-                default:
-                    break;
+    public static int countNumbers(String str){
+        int lettersCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                lettersCount++;
             }
         }
+        return lettersCount;
     }
 
-    public static int blink(int bulbs){
-        return ~bulbs;
+    public static boolean hasSpecialSymbols(String str){
+        return str.matches("[\\p{L}| ]+");
     }
 
-    public static int run(int bulbs){
-        return  bulbs >> 1;
-    }
-    public static boolean isFirstLampOn (int bulbs){
-        return (bulbs & 1) == 1;
-    }
-    public static void showBulbs(int bulbs){
-        System.out.println(Integer.toBinaryString(bulbs));
-    }
 
-    public static void Task1(){
-        for (int i = 2; i <= 100; ++i) {
-            if(checkSimple(i) == true) {
-                System.out.println(i);
-            }
-        }
-    }
-
-    public static boolean checkSimple(int i){
-        if (i<=1)
-            return false;
-        else if (i <=3)
-            return true;
-        else if (i%2==0 || i %3 ==0)
-            return false;
-        int n = 5;
-        while (n*n <=i){
-            if (i % n ==0 || i % (n+2) == 0)
-                return false;
-            n=n+6;
-        }
-        return true;
-    }
 }
+
