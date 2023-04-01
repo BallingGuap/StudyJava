c62f3a460b733d0f05d52022f6d4b2e9d1fbbf8d
sidiropulometin@gmail.com <sidiropulometin@gmail.com>
2023-04-01 20:23:55 +0600

    Homework №3

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,90 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Task1();
+        Task2();
     }
 
    public static void Task1() {
-       for(;;) {
-           Scanner scanner = new Scanner(System.in);
-           System.out.println("Type password:   ");
-           String pass = scanner.nextLine();
-
-           if(pass.length()  < 10){
-               System.out.println("Uncorrected password: " + pass);
-               continue;
-           }
-           if(hasSpecialSymbols(pass)){
-               System.out.println("Uncorrected password: " + pass);
-               continue;
-           }
-           if(countNumbers(pass) < 2){
-               System.out.println("Uncorrected password: " + pass);
-               continue;
-           }
-           System.out.println("Correct password: " + pass);
-       }
+       Bird bird = new Bird();
+       bird.walk();
+       bird.fly();
+       bird.sing();
    }
 
-    public static int countNumbers(String str){
+    public static void Task2() {
+        for(;;) {
+
+
+            var scanner = new Scanner(System.in);
+            System.out.println("Write first number!");
+            var number1 = scanner.nextLine();
+            if(!hasOnlyNumbers(number1)){
+                System.out.println("Incorrect format of number!");
+                continue;
+            }
+
+            System.out.println("Write second number!");
+            var number2 = scanner.nextLine();
+            if(!hasOnlyNumbers(number2)){
+                System.out.println("Incorrect format of number!");
+                continue;
+            }
+
+            Adder adder = new Adder(Integer.parseInt(number1),
+                                    Integer.parseInt(number2));
+            System.out.println("Result: " + adder.add());
+        }
+    }
+
+    public static boolean hasOnlyNumbers(String str){
         int lettersCount = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (Character.isDigit(str.charAt(i))) {
-                lettersCount++;
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
             }
         }
-        return lettersCount;
-    }
-
-    public static boolean hasSpecialSymbols(String str){
-        return str.matches("[\\p{L}| ]+");
+       return true;
     }
 
 
+
+
+
+}
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Animal{
+    void fly(){
+        System.out.println("I am flying");
+    }
+
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+
+ class Arithmetic{
+    protected int add(int n1, int n2){
+        return n1 + n2;
+    }
+}
+
+
+class Adder extends  Arithmetic{
+    private int _n1;
+    private int _n2;
+    Adder(int n1, int n2) {
+        _n1 = n1;
+        _n2 = n2;
+    }
+
+    public int add(){
+        return super.add(_n1,_n2);
+    }
 }
 
